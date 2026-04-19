89e78c3df88c2bd1070c76401bf621e01ef84383
Caio Felipe <caiofeli400@gmail.com>
2026-04-19 01:09:51 -0300

    Delete Trabalhos faculdade/atividade2.py

--- Trabalhos faculdade/atividade2.py
+++
@@ -1,8 +0,0 @@
-opcao =  int(input("Voce prefere praia ?" 
-" Digite 1 para SIM:"))
-
-if opcao == 1:
-    nome_praia = input("Digite o " 
-    "nome de uma praia:")
-    print ("Voce escolheu a praia:",
-            nome_praia) 
